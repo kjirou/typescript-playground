1caa775cc023f507821a3ba0775e05119bbe0de6
kjirou <sorenariblog@gmail.com>
2019-01-04 17:42:15 +0900

    Add a sample for --spressExcessPropertyErrors option

---
+++ examples/suppressExcessPropertyErrors-option.ts
@@ -0,0 +1,14 @@
+/*
+ * --suppressExcessPropertyErrors
+ *
+ * Ref) https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#strict-object-literal-assignment-checking
+ */
+
+// suppressExcessPropertyErrors がないと NG
+let x: { foo: number };
+x = { foo: 1, baz: 2 };
+
+// suppressExcessPropertyErrors なしでも OK
+// Object.assign を使うには "--lib ES2015" が必要
+let o: { x: number, y: number };
+o = Object.assign({x: 1}, {y: 2});
