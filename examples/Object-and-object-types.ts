cc0338552e94d5ba0b96f51d64ec337d2a110e6b
kjirou <sorenariblog@gmail.com>
2019-01-15 20:34:45 +0900

    Add a sample for the Object/object type

---
+++ examples/Object-and-object-types.ts
@@ -0,0 +1,20 @@
+//
+// Object
+//
+const o1: Object = 4;
+
+// OK: Object.prototype has `hasOwnProperty`.
+o1.hasOwnProperty('foo');
+// NG: Object.prototype does not have `toFixed`.
+o1.toFixed();
+
+
+//
+// object
+//
+const foo = (o: object) => {};
+
+// OK
+foo({});
+// NG
+foo(4);
