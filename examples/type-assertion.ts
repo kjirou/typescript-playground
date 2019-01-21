93c5abcc16e2265d35bd0819a1ca23dfe379d5d6
kjirou <sorenariblog@gmail.com>
2019-01-21 11:05:11 +0900

    Add a sample

---
+++ examples/type-assertion.ts
@@ -0,0 +1,21 @@
+class Foo {
+  getX() {}
+}
+
+class Bar extends Foo {
+  getY() {}
+}
+
+// NG: これらは may be a mistake って言われて無理
+const str1 = 'a' as number;
+const foo1 = new Foo() as number;
+
+// OK: 一般的にアップキャストと呼ばれ、安全と言われる
+let bar = new Bar() as Foo;
+bar.getX();  // OK
+bar.getY();  // NG: NG にしてくれるから安全
+
+// OK: 一般的にダウンキャストと呼ばれ、安全ではないと言われる
+let foo2 = new Foo() as Bar;
+foo2.getX();  // OK
+foo2.getY();  // OK: OK になっちゃうから安全ではない
