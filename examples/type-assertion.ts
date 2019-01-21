174d33f48135b91fe8d66e1ce2def2df826607b8
kjirou <sorenariblog@gmail.com>
2019-01-21 11:14:06 +0900

    Update a sample

--- examples/type-assertion.ts
+++ examples/type-assertion.ts
@@ -11,11 +11,16 @@ const str1 = 'a' as number;
 const foo1 = new Foo() as number;
 
 // OK: 一般的にアップキャストと呼ばれ、安全と言われる
-let bar = new Bar() as Foo;
-bar.getX();  // OK
-bar.getY();  // NG: NG にしてくれるから安全
+const bar1 = new Bar() as Foo;
+bar1.getX();  // OK
+bar1.getY();  // NG: NG にしてくれるから安全
 
 // OK: 一般的にダウンキャストと呼ばれ、安全ではないと言われる
-let foo2 = new Foo() as Bar;
+const foo2 = new Foo() as Bar;
 foo2.getX();  // OK
 foo2.getY();  // OK: OK になっちゃうから安全ではない
+
+// OK: こういう書き方もできる
+const bar2 = <Foo>new Bar();
+bar2.getX();  // OK
+bar2.getY();  // NG: 期待通り
