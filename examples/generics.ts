d7d1651f4a9ac4027d109db84403adf958184b73
kjirou <sorenariblog@gmail.com>
2019-01-31 13:23:29 +0900

    Update a sample

--- examples/generics.ts
+++ examples/generics.ts
@@ -9,3 +9,15 @@ function toXProp<T>(x: T): {x: T} {
 const o1: {x: number} = toXProp(1);
 // NG
 //const o2: {x: number} = toXProp('a');
+
+
+//
+// .call や .apply の呼び出しが --strictBindCallApply で変わる例
+//
+
+// NG: --strictBindCallApply なしなら OK
+//const ary1: number[] = Array.prototype.map.call([1, 2], v => v * v);
+
+// OK: 型引数で map の挙動を全部書く方法
+const ary2 = Array.prototype.map.call<number[], [(v: number) => number], number[]>([1, 2], v => v * v);
+const ary3 = Array.prototype.map.apply<number[], [(v: number) => number], number[]>([1, 2], [v => v * v]);
