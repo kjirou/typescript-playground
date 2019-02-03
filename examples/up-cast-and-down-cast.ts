8264f1df5b423fb2c08ce8352a37b5dfd5a4d5fd
kjirou <sorenariblog@gmail.com>
2019-02-03 14:09:02 +0900

    Add a sample

---
+++ examples/up-cast-and-down-cast.ts
@@ -0,0 +1,18 @@
+//
+// any のアップキャストに as 宣言しなくても良い。
+//
+const a: any = 1;
+// OK
+let b: number = a;
+// OK
+let c = a as number;
+
+//
+// any 以外のアップキャストは as が必要っぽい
+//
+const x: string | number = Math.random() > 0.5 ? 'a' : 1;
+// NG
+//let y: string = x;
+// OK
+let z = x as number;
+z.toFixed();
