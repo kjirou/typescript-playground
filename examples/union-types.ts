fc1152594758a584f2c4a0a47b74e7de62982dc0
kjirou <sorenariblog@gmail.com>
2019-02-04 17:40:51 +0900

    Update a sample

--- examples/union-types.ts
+++ examples/union-types.ts
@@ -9,3 +9,28 @@
 //
 const x: string | number = 1;
 x.toFixed();
+
+
+//
+// keyof object による動的な型生成
+//
+interface O {
+  x: number,
+  y: number,
+}
+const o1: O = {
+  x: 1,
+  y: 2,
+};
+let k: keyof O = 'x';
+// OK
+k = 'y';
+// NG
+//k = 'z';
+
+const o2 = {
+  x: 1,
+  y: 2,
+};
+// NG: 変数から直接は無理っぽい
+//let l: keyof o2;
