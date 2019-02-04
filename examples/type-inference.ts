57eb879496f19a2fca18bc730ab6fde0cb82118f
kjirou <sorenariblog@gmail.com>
2019-02-04 18:06:32 +0900

    Add a sample

---
+++ examples/type-inference.ts
@@ -0,0 +1,30 @@
+/*
+ * 型推論
+ */
+
+//
+// typeof と if の違い
+//
+function identify1(arg: string | null) {
+  return arg;
+}
+// NG
+//identify1('a').toUpperCase();
+
+function identify2(arg: string | null) {
+  if (arg !== null) {
+    return arg;
+  }
+  return '';
+}
+// OK
+identify2('a').toUpperCase();
+
+function identify3(arg: string | null) {
+  if (typeof arg === 'string') {
+    return arg;
+  }
+  return '';
+}
+// OK
+identify3('a').toUpperCase();
