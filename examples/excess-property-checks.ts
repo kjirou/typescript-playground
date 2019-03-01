8183b1814e5b0fd39578d6bfc347e402e085d600
kjirou <sorenariblog@gmail.com>
2019-03-01 11:26:21 +0900

    Add an example

---
+++ examples/excess-property-checks.ts
@@ -0,0 +1,15 @@
+/**
+ * Ref) https://www.typescriptlang.org/docs/handbook/interfaces.html#excess-property-checks
+ */
+
+type Excess = {
+  x: number,
+  [key: string]: any,
+}
+
+const o: Excess = {x: 1};
+o.y = 'a';
+o.z = null;
+
+// NG
+//o.x = 'a';
