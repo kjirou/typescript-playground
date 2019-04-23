7867e0d5d542a46787fa201c088eadea655ab4e0
kjirou <sorenariblog@gmail.com>
2019-04-23 11:46:20 +0900

    Add an example

---
+++ examples/tuple.ts
@@ -0,0 +1,14 @@
+/*
+ * Ref) https://www.typescriptlang.org/docs/handbook/basic-types.html の "Tuple" セクション
+ */
+
+//
+// 以下ドキュメントから
+//
+
+// Declare a tuple type
+let x: [string, number];
+// Initialize it
+x = ["hello", 10]; // OK
+// Initialize it incorrectly
+x = [10, "hello"]; // Error
