7d889948008988f30936eb2ab238e79c658274b9
kjirou <sorenariblog@gmail.com>
2019-07-19 15:50:29 +0900

    Add an example

---
+++ examples/function-typing-variations.ts
@@ -0,0 +1,13 @@
+/*
+ * 忘れがちな様々な関数構文に対する型の付け方
+ */
+
+const foo = (a: number, b: number): number => {
+  return a + b;
+};
+foo(1, 2);
+
+const bar: (a: number, b: number) => number = (a, b) => {
+  return a + b;
+};
+bar(1, 2);
