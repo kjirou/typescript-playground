513b8f9d5c82b2953d7556de6b534171deb3f710
kjirou <sorenariblog@gmail.com>
2019-09-17 11:41:52 +0900

    理由が多分わかった。

--- examples/object-spread.ts
+++ examples/object-spread.ts
@@ -22,21 +22,12 @@ const point3Dc = {...point2D, z: 3, foo: 1};
 // NG
 //const point3Dd: Point3D = {...point2D, z: 3, foo: 1};
 
-// OK: "xx" は存在しないので NG になって欲しかったけど通る。理由は不明。
+// OK: "xx" は存在しないので NG になって欲しかったけど通る。
+//     おそらくは、Object Spreading により `{x: number, y: number, xx: number} | {x: number, y: number}` と
+//       推論された値を Point2D だと明示的に宣言しているので、明示的な宣言を信じて後者と解釈されている。
 const flag = true;
 const invalidPoint2D: Point2D = {
   x: 1,
   y: 2,
   ...(flag ? {xx: 11} : {})
 };
-// NG: 一方で下記はもちろん落とされる
-//const invalidPoint2D2: Point2D = flag
-//  ? {
-//    x: 1,
-//    y: 2,
-//    xx: 11,
-//  }
-//  : {
-//    x: 1,
-//    y: 2,
-//  };
