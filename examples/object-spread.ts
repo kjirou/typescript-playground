fc212545d3dcc096e261bdc381c31761c26e4a6c
kjirou <sorenariblog@gmail.com>
2019-09-17 11:30:25 +0900

    Object spread をしたときに期待通りにならない件をメモ

--- examples/object-spread.ts
+++ examples/object-spread.ts
@@ -9,7 +9,6 @@ type Point3D = {
 };
 
 const point2D: Point2D = {x: 1, y: 2};
-
 const point3D = {...point2D, z: 3};
 
 // NG
@@ -22,3 +21,22 @@ const point3Dc = {...point2D, z: 3, foo: 1};
 
 // NG
 //const point3Dd: Point3D = {...point2D, z: 3, foo: 1};
+
+// OK: "xx" は存在しないので NG になって欲しかったけど通る。理由は不明。
+const flag = true;
+const invalidPoint2D: Point2D = {
+  x: 1,
+  y: 2,
+  ...(flag ? {xx: 11} : {})
+};
+// NG: 一方で下記はもちろん落とされる
+//const invalidPoint2D2: Point2D = flag
+//  ? {
+//    x: 1,
+//    y: 2,
+//    xx: 11,
+//  }
+//  : {
+//    x: 1,
+//    y: 2,
+//  };
