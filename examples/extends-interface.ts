5f5e1e8c6928761fd254efd5609aab06e9a5c5e4
kjirou <sorenariblog@gmail.com>
2019-10-09 14:53:11 +0900

    Add a sample

---
+++ examples/extends-interface.ts
@@ -0,0 +1,11 @@
+interface Foo {
+  xyz?: 'x' | 'y' | 'z';
+}
+
+interface Bar extends Foo {
+  x: number,
+}
+
+const foo1: Foo = {xyz: 'x'};
+const foo2: Foo = {xyz: undefined};
+const foo3: Foo = {};
