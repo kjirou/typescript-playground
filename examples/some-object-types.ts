ab12f7e57e57b2bb1c91a20482e86390fa3f712e
kjirou <sorenariblog@gmail.com>
2020-03-23 17:44:34 +0900

    Add a sample

---
+++ examples/some-object-types.ts
@@ -0,0 +1,29 @@
+// {}
+const obj: {} = {x: 1, y: 2}
+const ary: {} = []
+const str: {} = 'a'
+const num: {} = 1
+const bool: {} = true
+// 下の 2 つが NG
+//const nul: {} = null
+//const undef: {} = undefined
+
+// Object
+const obj2: Object = {x: 1, y: 2}
+const ary2: Object = []
+const str2: Object = 'a'
+const num2: Object = 1
+const bool2: Object = true
+// 下の 2 つが NG
+//const nul2: Object = null
+//const undef2: Object = undefined
+
+// object
+const obj3: object = {x: 1, y: 2}
+const ary3: object = []
+// 下の 5 つが NG
+//const str3: object = 'a'
+//const num3: object = 1
+//const bool3: object = true
+//const nul3: object = null
+//const undef3: object = undefined
