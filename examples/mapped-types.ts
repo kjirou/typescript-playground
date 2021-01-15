1b76b07d131fda9417bc0a02563622fbcc532640
kjirou <sorenariblog@gmail.com>
2021-01-15 13:54:25 +0900

    Update a sample

--- examples/mapped-types.ts
+++ examples/mapped-types.ts
@@ -10,4 +10,16 @@ const m1: Mapped1 = {
 //  x: 1,
 //};
 
+type NumericKeyOnly<K extends number, T> = { [P in K]: T }
+type HttpStatusTexts = NumericKeyOnly<200 | 400 | 404, string>
+const foo: HttpStatusTexts = {
+  200: 'OK',
+  400: 'Bad Request',
+  404: 'Not Found',
+  // NG
+  //500: 'Internal Server Error',
+  // NG
+  //'200': 'Hi',
+}
+
 export {}
